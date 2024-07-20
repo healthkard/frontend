7760c2ad0f096566afd601b2b7068e7af29bca1e
Akram <akramshaik1206@gmail.com>
2024-07-20 22:34:27 +0530

    Button complete

--- src/component/Button/index.js
+++ src/component/Button/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
-function Button() {
+function Button({ color, onClick, label, width='10%'  }) {
+    
+  const baseClasses = 'px-4 py-2 font-semibold rounded-md shadow-md';
   return (
     
-      <button>submit</button>
+      <button
+
+        onClick={onClick}
+        style={{width}}
+        className={`${color} ${baseClasses}`}
+      
+      >{label}</button>
     
   )
 }
